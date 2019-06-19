56820e5b6e2e03ea6195af2a2bc6a2b1747e50aa
Harsh Mehta <deadspaceharsh@gmail.com>
2019-06-19 17:36:37 +0530

    added header/footer

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>  
+                <hr/>
+                <center>
+                This source code is licensed <a href="https://github.com/harshil1712/mozguj/blob/master/LICENSE">MIT</a>
+                <br/>Found a bug? <a href="https://github.com/harshil1712/mozguj/issues">Report or fix it</a>
+                </center>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,31 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => {
+  return(
+    <div>
+      <h1><Link to="/">MozGuj</Link></h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>  
+          </li>
+          <li>
+            <Link to="/">Events</Link> 
+          </li>
+          <li>
+            <Link to="/">Blog</Link>  
+          </li>
+          <li>
+            <Link to="/">Contact</Link>  
+          </li>
+          <li>
+            <Link to="/">About Us</Link>  
+          </li> 
+        </ul>
+      </nav>
     </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  )
 }
 
 export default Header
