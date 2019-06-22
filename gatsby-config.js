205ac390fd522a2b5b44e643a2fc54a1686f3171
Harshil Agrawal <ghagrawal17@gmail.com>
2019-06-22 19:28:47 +0530

    basic masonry

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    "gatsby-transformer-json",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -25,6 +26,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./data/",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,64 @@
-import React from "react"
-import { Link } from "gatsby"
+// import React from "react"
+// import { Link } from "gatsby"
 
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+//     <Link to="/page-2/">Go to page 2</Link>
+//   </Layout>
+// )
+
+// export default IndexPage
+
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Masonry from 'react-masonry-component'
+import Img from 'gatsby-image'
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Masonry>
+      {data.allImagesJson.edges.map(({ node }) => (
+        <div key={node.id}>
+              <Img fixed={node.image.childImageSharp.fixed}/>
+        </div>
+      ))}
+    </Masonry>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexQuery {
+    allImagesJson {
+      edges {
+        node {
+          id
+          title
+          image {
+            id
+            childImageSharp {
+              fixed (width:300){
+                src
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          caption
+        }
+      }
+    }
+  }
+`
