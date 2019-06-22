a7159b25defa5d241ae30db26cc7a06f43c7b13a
Harshil Agrawal <ghagrawal17@gmail.com>
2019-06-22 19:28:47 +0530

    basic masonry

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mozilla Gujarat`,
+    description: `We are Mozilla Gujarat! A highly enthusiastic community of Mozilla lovers in Gujarat, India`,
+    author: `Mozilla Gujarat`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    "gatsby-transformer-json",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -25,6 +26,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./data/",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
