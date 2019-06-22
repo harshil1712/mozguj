551c98d7f2e2714cda27495b4a672a8cfeb1ffce
Harshil Agrawal <ghagrawal17@gmail.com>
2019-06-22 19:49:15 +0530

    added prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mozilla Gujarat`,
+    description: `We are Mozilla Gujarat! A highly enthusiastic community of Mozilla lovers in Gujarat, India`,
+    author: `Mozilla Gujarat`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
