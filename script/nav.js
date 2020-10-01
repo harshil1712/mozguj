a648a00577ea5d8649d5f00fabf89046d93eb5a6
Rushik Ramani <rush.ramani@gmail.com>
2020-10-01 22:49:17 +0530

    navbar changes

--- script/nav.js
+++ script/nav.js
@@ -2,3 +2,13 @@ $( '.navbar-nav a' ).on( 'click', function () {
 	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 });
+
+// onscroll border bottom
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 0) {
+        $('.navbar').addClass('sticky-nav');
+    } else {
+        $('.navbar').removeClass('sticky-nav');
+    }
+});
